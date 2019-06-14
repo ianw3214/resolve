b61459e69a6ecfab71a8e4b362833c9d739876f8
Ian <ianw3214@gmail.com>
2019-06-13 22:16:59 -0400

    Setup text rendering to take in custom fonts

--- graphics/text.js
+++ graphics/text.js
@@ -28,7 +28,11 @@ const text_fragment = `
 
 // TODO: Separate into JSON file
 // The default font info
-let fontInfo = {
+let defaultFont = {
+    // ACTUAL TEXTURE DATA
+    glyphTex: null,
+    textBufferInfo: {},
+    // FONT METADATA
     letterHeight: 8,
     spaceWidth: 8,
     spacing: -1,
@@ -79,8 +83,6 @@ let fontInfo = {
 };
 
 graphics.text = {
-    glyphTex: null,
-    textBufferInfo: {}
 };
 
 graphics.text.init = function() {
@@ -91,15 +93,15 @@ graphics.text.init = function() {
     gl.uniform1f(gl.getUniformLocation(text_shader, "u_screenHeight"), gl.canvas.clientHeight);
 
     // Create a texture.
-    graphics.text.glyphTex = gl.createTexture();
-    gl.bindTexture(gl.TEXTURE_2D, graphics.text.glyphTex);
+    defaultFont.glyphTex = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, defaultFont.glyphTex);
     
     // Asynchronously load an image
     let image = new Image();
     image.src = "res/fonts/font.png";
     image.onload = function () {
         // Now that the image has loaded make copy it to the texture.
-        gl.bindTexture(gl.TEXTURE_2D, graphics.text.glyphTex);
+        gl.bindTexture(gl.TEXTURE_2D, defaultFont.glyphTex);
         gl.pixelStorei(gl.UNPACK_PREMULTIPLY_ALPHA_WEBGL, true);
 
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
@@ -110,7 +112,7 @@ graphics.text.init = function() {
     };
 
     // Maunally create a bufferInfo
-    graphics.text.textBufferInfo = {
+    defaultFont.textBufferInfo = {
         attribs: {
             a_position: { buffer: gl.createBuffer(), numComponents: 2, },
             a_texcoord: { buffer: gl.createBuffer(), numComponents: 2, },
@@ -119,36 +121,36 @@ graphics.text.init = function() {
     };
 }
 
-function makeVerticesForString(fontInfo, str, x, y, size) {
+function makeVerticesForString(font, str, x, y, size) {
     let len = str.length;
     let numVertices = len * 6;
     let positions = new Float32Array(numVertices * 2);
     let texcoords = new Float32Array(numVertices * 2);
     let offset = 0;
-    let maxX = fontInfo.textureWidth;
-    let maxY = fontInfo.textureHeight;
+    let maxX = font.textureWidth;
+    let maxY = font.textureHeight;
 
     // Loop through each letter of the string
     for (let i = 0; i < len; ++i) {
         let letter = str[i];
-        let glyphInfo = fontInfo.glyphInfos[letter];
+        let glyphInfo = font.glyphInfos[letter];
 
         // Only generate the glyph if info for it exists
         if (glyphInfo) {
-            let x2 = x + glyphInfo.width * (size / fontInfo.letterHeight);
+            let x2 = x + glyphInfo.width * (size / font.letterHeight);
             let u1 = glyphInfo.x / maxX;
-            let v1 = (glyphInfo.y + fontInfo.letterHeight - 1) / maxY;
+            let v1 = (glyphInfo.y + font.letterHeight - 1) / maxY;
             let u2 = (glyphInfo.x + glyphInfo.width - 1) / maxX;
             let v2 = glyphInfo.y / maxY;
 
             // 6 vertices per letter
             positions[offset + 0] = x;
-            positions[offset + 1] = y + fontInfo.letterHeight * (size / fontInfo.letterHeight);
+            positions[offset + 1] = y + font.letterHeight * (size / font.letterHeight);
             texcoords[offset + 0] = u1;
             texcoords[offset + 1] = v1;
 
             positions[offset + 2] = x2;
-            positions[offset + 3] = y + fontInfo.letterHeight * (size / fontInfo.letterHeight);
+            positions[offset + 3] = y + font.letterHeight * (size / font.letterHeight);
             texcoords[offset + 2] = u2;
             texcoords[offset + 3] = v1;
 
@@ -163,7 +165,7 @@ function makeVerticesForString(fontInfo, str, x, y, size) {
             texcoords[offset + 7] = v2;
 
             positions[offset + 8] = x2;
-            positions[offset + 9] = y + fontInfo.letterHeight * (size / fontInfo.letterHeight);
+            positions[offset + 9] = y + font.letterHeight * (size / font.letterHeight);
             texcoords[offset + 8] = u2;
             texcoords[offset + 9] = v1;
 
@@ -172,11 +174,11 @@ function makeVerticesForString(fontInfo, str, x, y, size) {
             texcoords[offset + 10] = u2;
             texcoords[offset + 11] = v2;
 
-            x += glyphInfo.width * (size / fontInfo.letterHeight) + fontInfo.spacing;
+            x += glyphInfo.width * (size / font.letterHeight) + font.spacing;
             offset += 12;
         } else {
             // we don't have this character so just advance
-            x += fontInfo.spaceWidth;
+            x += font.spaceWidth;
         }
     }
 
@@ -191,34 +193,34 @@ function makeVerticesForString(fontInfo, str, x, y, size) {
     };
 }
 
-graphics.text.drawText = function(text, x = 0, y = 0, size = 16) {
+graphics.text.drawText = function(text, font = defaultFont, x = 0, y = 0, size = 16) {
 
     if (typeof text !== "string") {
-        // TODO: Better error handling, maybe output into log file?
+        logger.error("Unable to draw text: " + text);
         return;
     }
 
     gl.useProgram(text_shader);
-    gl.bindTexture(gl.TEXTURE_2D, graphics.text.glyphTex);
+    gl.bindTexture(gl.TEXTURE_2D, font.glyphTex);
 
     // TODO: Determine if toLowerCase is needed based on font info
-    let vertices = makeVerticesForString(fontInfo, text.toLowerCase(), x, y, size);
+    let vertices = makeVerticesForString(font, text.toLowerCase(), x, y, size);
 
     let positionLocation = gl.getAttribLocation(text_shader, "a_pos");
     let texCoordLocation = gl.getAttribLocation(text_shader, "a_tex");
 
     // update the buffers
-    graphics.text.textBufferInfo.attribs.a_position.numComponents = 2;
-    gl.bindBuffer(gl.ARRAY_BUFFER, graphics.text.textBufferInfo.attribs.a_position.buffer);
+    font.textBufferInfo.attribs.a_position.numComponents = 2;
+    gl.bindBuffer(gl.ARRAY_BUFFER, font.textBufferInfo.attribs.a_position.buffer);
     gl.bufferData(gl.ARRAY_BUFFER, vertices.arrays.position, gl.DYNAMIC_DRAW);
-    gl.bindBuffer(gl.ARRAY_BUFFER, graphics.text.textBufferInfo.attribs.a_texcoord.buffer);
+    gl.bindBuffer(gl.ARRAY_BUFFER, font.textBufferInfo.attribs.a_texcoord.buffer);
     gl.bufferData(gl.ARRAY_BUFFER, vertices.arrays.texcoord, gl.DYNAMIC_DRAW);
 
     gl.enableVertexAttribArray(positionLocation);
-    gl.bindBuffer(gl.ARRAY_BUFFER, graphics.text.textBufferInfo.attribs.a_position.buffer);
+    gl.bindBuffer(gl.ARRAY_BUFFER, font.textBufferInfo.attribs.a_position.buffer);
     gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(texCoordLocation);
-    gl.bindBuffer(gl.ARRAY_BUFFER, graphics.text.textBufferInfo.attribs.a_texcoord.buffer);
+    gl.bindBuffer(gl.ARRAY_BUFFER, font.textBufferInfo.attribs.a_texcoord.buffer);
     gl.vertexAttribPointer(texCoordLocation, 2, gl.FLOAT, false, 0, 0);
 
 
