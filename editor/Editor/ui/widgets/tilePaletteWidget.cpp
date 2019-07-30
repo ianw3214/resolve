ca8f5fc342bac1efb1790ae18089499860020e24
Ian <ianw3214@gmail.com>
2019-07-29 22:25:53 -0400

    Implement tile palette widget max width

--- editor/Editor/ui/widgets/tilePaletteWidget.cpp
+++ editor/Editor/ui/widgets/tilePaletteWidget.cpp
@@ -6,17 +6,18 @@
 using json = nlohmann::json;
 
 TilePaletteWidget::TilePaletteWidget(Editor * editor, const std::string& map_src) 
-    : editorRef(editor)
-    , map_src(map_src)
+    : m_editor_ref(editor)
+    , m_map_src(map_src)
     , Widget("Tile Palette", Anchor::BOTTOM_LEFT) 
 {
     // TODO: Make this more elegant somehow?
-    std::ifstream file(map_src);
+    std::ifstream file(m_map_src);
     if (file.is_open()) {
         json data;
         file >> data;
 
         json tilesheet = data["tilesheet"];
+        // TODO: Don't hard code this "../"
         m_tilesheet_src = std::string("../") + static_cast<std::string>(tilesheet["source"]);
         m_tile_size = tilesheet["tile_size"];
         m_tilesheet_width = tilesheet["width"];
@@ -27,6 +28,7 @@ TilePaletteWidget::TilePaletteWidget(Editor * editor, const std::string& map_src
 }
 
 void TilePaletteWidget::define() {
+    int curr_x = 0;
     for (int y = 0; y < m_tilesheet_height; ++y) {
         for (int x = 0; x < m_tilesheet_width; ++x) {
             // TODO: No magic numbers
@@ -36,9 +38,15 @@ void TilePaletteWidget::define() {
                 64, 
                 {x * m_tile_size, y * m_tile_size, m_tile_size, m_tile_size},
                 [=]() {
-                    editorRef->set_brush_tile(y * m_tilesheet_width + x);
+                    m_editor_ref->set_brush_tile(y * m_tilesheet_width + x);
                 }
             );
+            curr_x += 64;
+            // TODO: Fix this - don't use new line (or make new line also work with images)
+            if (curr_x >= max_width) {
+                curr_x = 0;
+                new_line(TextSize::LARGE);
+            }
         }
     }
 }
--- editor/Editor/ui/widgets/tilePaletteWidget.hpp
+++ editor/Editor/ui/widgets/tilePaletteWidget.hpp
@@ -5,14 +5,18 @@
 
 class Editor;
 class TilePaletteWidget : public Widget {
+
+    // TODO: This can probably be part of the actual widget system, scalable width/height
+    static const int max_width = 200;
+
 public:
     // TODO: Don't need separate map_src, can probably get straight from editor
     TilePaletteWidget(Editor * editor, const std::string & map_src);
     virtual void define() override;
 private:
-    Editor * editorRef;
+    Editor * m_editor_ref;
 
-    std::string map_src;
+    std::string m_map_src;
     std::string m_tilesheet_src;
     int m_tile_size;
     int m_tilesheet_width;
