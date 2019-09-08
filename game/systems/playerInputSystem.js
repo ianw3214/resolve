a1ac9bf418a244614f804bcff1264472f430ae48
Ian <ianw3214@gmail.com>
2019-09-08 17:49:08 -0400

    Re-implement attack system

--- game/systems/playerInputSystem.js
+++ game/systems/playerInputSystem.js
@@ -6,23 +6,33 @@ let playerInputSystem = {
         if (playerInputSystem.player !== null) {
             let player = playerInputSystem.player;
             if (player.hasOwnProperty("move")) {
+                let direction = 0;
                 if (input.keyPressed(68) || input.keyPressed(39)) {
                     player.move.right = true;
+                    direction = 1;
                 }
                 if (input.keyPressed(65) || input.keyPressed(37)) {
                     player.move.left = true;
+                    direction = 3;
                 }
                 if (input.keyPressed(87) || input.keyPressed(38)) {
                     player.move.up = true;
+                    direction = 0;
                 }
                 if (input.keyPressed(83) || input.keyPressed(40)) {
                     player.move.down = true;
+                    direction = 2;
                 }
+                // Let the player face a direction if possible
+                player.position["facing"] = direction;
             }
             if (player.hasOwnProperty("attack")) {
                 if (input.keyPressed(49)) {
                     // BASIC ATTACK
-                    player.attack.attacks.push("basic");
+                    player.attack.input_attack = {
+                        "key" : "basic",
+                        "direction" : player.position["facing"]
+                    };
                 }
             }
         } else {
@@ -31,6 +41,7 @@ let playerInputSystem = {
                 if (target.hasOwnProperty("player_input") === true) {
                     if (target.player_input.affect === true) {
                         playerInputSystem.player = target;
+                        playerInputSystem.player.position["facing"] = 0;
                         break;
                     }   
                 }
